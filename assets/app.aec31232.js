4f863c8cbce82f2dc41ef0c60c71e923c42da470
Oleksandr Semenov <forb.alex@gmail.com>
2023-12-08 00:56:36 +0200

    Updates

---
+++ assets/app.aec31232.js
@@ -0,0 +1,1 @@
+import{ax as e}from"./entry.97cc0967.js";const t=e("app",{state:()=>({currentPage:1,limit:10,searchQuery:""})});export{t as u};
--- assets/app.b0ea3575.js
+++
@@ -1,1 +0,0 @@
-import{ax as e}from"./entry.a225c3d4.js";const t=e("app",{state:()=>({currentPage:1,limit:10,searchQuery:""})});export{t as u};
